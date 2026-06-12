0de6f16f4f85e8853b03c2e5e48dd40e6872fb34
michaelbag8 <michaelbag8@gmail.com>
2026-06-12 15:53:45 +0100

    car function added

--- main.go
+++ main.go
@@ -10,3 +10,20 @@ type Car struct {
 	Year  int
 	Price float64
 }
+
+func allCars(inventory []Car) {
+	for _, cars := range inventory {
+		fmt.Printf("%s %s (%d)-$%.2f\n", cars.Brand, cars.Model, cars.Year, cars.Price)
+	}
+
+}
+
+func filterByCarBrand(inventory []Car, brand string) []Car {
+	var result []Car
+	for _, cars := range inventory {
+		if cars.Brand == brand {
+			result = append(result, cars)
+		}
+	}
+	return result
+}
